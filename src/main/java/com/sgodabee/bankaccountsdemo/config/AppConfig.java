64442654ffefeae315f258048cec30f178b37e43
Sgodabee <70349942+Sgodabee@users.noreply.github.com>
2021-12-11 22:19:38 +0200

    update 11 December 2021

--- src/main/java/com/sgodabee/bankaccountsdemo/config/AppConfig.java
+++ src/main/java/com/sgodabee/bankaccountsdemo/config/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@ComponentScan(basePackages = {"com.sgodabee.bankaccountdemo_v1"})
+@ComponentScan(basePackages = {"com.sgodabee.bankaccountsdemo"})
 
 public class AppConfig extends WebMvcConfigurationSupport{
 
